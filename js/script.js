abc5a7aefa734db309fab48d4fa04d5b02f96e28
Max <zixel37@gmail.com>
2019-08-24 04:07:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.slider_block').slick({
+        
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>'
+        
+    });
+
+    // $('.center').slick({
+    //     centerMode: true,
+    //     centerPadding: '60px',
+    //     slidesToShow: 3,
+    //     responsive: [
+    //       {
+    //         breakpoint: 768,
+    //         settings: {
+    //           arrows: false,
+    //           centerMode: true,
+    //           centerPadding: '40px',
+    //           slidesToShow: 3
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           arrows: false,
+    //           centerMode: true,
+    //           centerPadding: '40px',
+    //           slidesToShow: 1
+    //         }
+    //       }
+    //     ]
+    //   });
+
+});
+
